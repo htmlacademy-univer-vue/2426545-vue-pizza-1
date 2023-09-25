400ed6ff3a3f67669eefd9b9a38741bc187bad1c
BearPadIO <101512663+arandich@users.noreply.github.com>
2023-09-25 20:26:48 +0300

    - Verstochka added

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,15 @@
+<script>
+import { defineComponent } from "vue";
+import AppHeader from "./AppHeader.vue";
+
+export default defineComponent({
+  name: "AppLayout",
+  components: {AppHeader},
+});
+</script>
+
+<template>
+  <app-header></app-header>
+</template>
+
+<style scoped lang="scss"></style>
