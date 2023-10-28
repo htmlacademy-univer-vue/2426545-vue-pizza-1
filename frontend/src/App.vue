550f3e8f9c27002f8b0b1a618ece8bae824e6915
BearPadIO <101512663+arandich@users.noreply.github.com>
2023-10-28 15:00:27 +0300

    Я сделал все в одном коммите, поэтому 3 пулреквеста одинаковы :d

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,10 @@
 <template>
-<!--  <div class="main__wrapper">-->
-<!--    <div class="main__header">-->
-<!--      <img-->
-<!--        src="../src/assets/img/logo.svg"-->
-<!--        width="300"-->
-<!--        height="47"-->
-<!--        alt="V!U!E! Pizza"-->
-<!--      />-->
-<!--    </div>-->
-<!--    <h1>Добро пожаловать!</h1>-->
-<!--    <p>-->
-<!--      Это проект V!U!E! Pizza для обучения на профессиональном онлайн‑курсе<br />-->
-<!--      <b>«Vue.js для опытных разработчиков».</b>-->
-<!--    </p>-->
-<!--  </div>-->
-<app-layout></app-layout>
+  <app-layout></app-layout>
 </template>
 
+<script setup>
+import AppLayout from "./layouts/AppLayout.vue";
+</script>
 <style lang="scss">
 //@import "@/assets/scss/app.scss";
 @import "@/assets/scss/style.scss";
@@ -66,6 +54,3 @@ body {
   }
 }
 </style>
-<script setup>
-import AppLayout from "./layouts/AppLayout.vue";
-</script>
