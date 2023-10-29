d6c146dc02eba0edb6b57cc2566344b2674e23c2
BearPadIO <101512663+arandich@users.noreply.github.com>
2023-10-29 14:10:35 +0300

    Router added

---
+++ frontend/src/layouts/AppLayoutDefault.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app_layout">
+    <slot />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.app_layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.content {
+  display: flex;
+  flex-grow: 1;
+}
+</style>
