94752365c7cda4a5bfad27ea93d4d75e3847b410
BearPadIO <101512663+arandich@users.noreply.github.com>
2023-11-07 22:54:09 +0300

    States for stores

--- frontend/src/stores/data.js
+++ frontend/src/stores/data.js
@@ -1,15 +1,27 @@
 import { defineStore } from "pinia";
 
 export const useDataStore = defineStore("data", {
-    state: () => ({
-        data: {}
-    }),
-    getters: {
-        getData: (state) => state.data
-    },
-    actions: {
-        setData(data) {
-            this.data = data
-        }
-    }
-})
+  state: () => {
+    return {
+      doughs: getData("dough.json"),
+      sizes: getData("sizes.json"),
+      ingredients: getData("ingredients.json"),
+      misc: getData("misc.json"),
+      sauces: getData("sauces.json"),
+    };
+  },
+  getters: {
+    getDoughs: (state) => state.doughs,
+    getSizes: (state) => state.sizes,
+    getIngredients: (state) => state.ingredients,
+    getMisc: (state) => state.misc,
+    getSauces: (state) => state.sauces,
+  },
+  actions: {},
+});
+
+async function getData(path) {
+  // TODO Swap to server request
+  const response = await fetch(`../mocks/${path}`);
+  return await response.json();
+}
--- frontend/src/stores/pizza.js
+++ frontend/src/stores/pizza.js
@@ -1,16 +1,46 @@
-import {defineStore} from "pinia";
-
+import { defineStore } from "pinia";
 
 export const usePizzaStore = defineStore("pizza", {
-    state: () => ({
-        pizzas: []
-    }),
-    getters: {
-        getPizzas: (state) => state.pizzas
+  state: () => ({
+    pizza: {
+      name: "",
+      dough: {},
+      size: {},
+      sauce: {},
+      ingredients: [],
     },
-    actions: {
-        addPizza(pizza) {
-            this.pizzas.push(pizza)
+  }),
+  getters: {
+    getPizza: (state) => state.pizza,
+    getPizzaIngredients: (state) => state.pizza.ingredients,
+    getPizzaSize: (state) => state.pizza.size,
+    getPizzaSauce: (state) => state.pizza.sauce,
+    getPizzaDough: (state) => state.pizza.dough,
+    getPizzaName: (state) => state.pizza.name,
+  },
+  actions: {
+    // ADD Objects to pizza
+    setDoughToPizza(dough) {
+      this.pizza.dough = dough;
+    },
+    setSizeToPizza(size) {
+      this.pizza.size = size;
+    },
+    setSauceToPizza(sauce) {
+      this.pizza.sauce = sauce;
+    },
+    addIngredientToPizza(ingredient) {
+      const existingIngredient = this.pizza.ingredients.find(
+        (item) => item.id === ingredient.id
+      );
+
+      if (existingIngredient) {
+        if (existingIngredient.quantity < 3) {
+          existingIngredient.quantity += 1;
         }
-    }
-})
+      } else {
+        this.pizza.ingredients.push({ ...ingredient, quantity: 1 });
+      }
+    },
+  },
+});
